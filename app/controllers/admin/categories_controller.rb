e01ad60cff0da91e34a45d9da9c72645003c2f6d
Keith Millar <keithmillar@hotmail.ca>
2020-07-29 01:28:01 +0000

    index has table with categories and number of products in each

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,6 +1,12 @@
 class Admin::CategoriesController < ApplicationController
   def show
   end
+  
   def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
   end
 end
