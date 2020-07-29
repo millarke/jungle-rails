487fedb3b856e1525132afe66c34f66676c6bba9
Keith Millar <keithmillar@hotmail.ca>
2020-07-29 02:21:03 +0000

    can now add categories from the admin/categories/news page

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -9,4 +9,20 @@ class Admin::CategoriesController < ApplicationController
   def new
     @category = Category.new
   end
+
+  def create
+    @article = Category.new(category_params)
+  
+    if @article.save
+      redirect_to [:admin, :categories]
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
